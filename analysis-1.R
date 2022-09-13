86bba15305f861ec149fdaf85259ea308c64a5cc
Matt Kmiecik <mkmiecik14@gmail.com>
2022-09-13 17:00:38 -0500

    added analysis and api calls

--- analysis-1.R
+++ analysis-1.R
@@ -13,90 +13,58 @@ load("../output/iaf-res.rda") # loads data
 iaf_res_long <- 
   iaf_res %>% 
   pivot_longer(c(-ss, -block, -eyes)) %>%
-  mutate(eyes = factor(eyes))
-
-# Sets contrast (open > closed)
-contrasts(iaf_res_long$eyes) <- cbind(open_vs_closed = c(-.5, .5))
-
-# Linear mixed effects modeling
-
-# PEAK ALPHA FREQUENCY (via peak picking)
-
-# Minimal model
-mod_1_paf <- 
-  lmer(
-    value ~ 1 + block + eyes + (1 | ss), 
-    data = iaf_res_long %>% filter(name == "paf"),
-    REML = TRUE
+  mutate(
+    eyes = factor(eyes), # turns into a factor 
+    block_mc = block - 4.5 # mean centers
   )
-summary(mod_1_paf)
-check_model(mod_1_paf)
 
-mod_2_paf <-
-  lmer(
-    value ~ 1 + block + eyes + (1 + block + eyes | ss), 
-    data = iaf_res_long %>% filter(name == "paf"),
-    REML = TRUE
-  )
-summary(mod_2_paf)
-check_model(mod_2_paf)
+# proof that it is mean centered
+iaf_res_long %>% 
+  group_by(ss, name) %>% 
+  summarise(m = mean(block_mc)) %>% 
+  ungroup() %>% 
+  filter(m != 0)
 
-# Model comparison
-anova(mod_1_paf, mod_2_paf)
-
-# CENTER OF GRAVITY (essentially a weighted average)
-
-# Minimal model
-mod_1_cog <- 
-  lmer(
-    value ~ 1 + block + eyes + (1 | ss), 
-    data = iaf_res_long %>% filter(name == "cog"),
-    REML = TRUE
-  )
-summary(mod_1_cog)
-check_model(mod_1_cog)
-
-mod_2_cog <-
-  lmer(
-    value ~ 1 + block + eyes + (1 + block + eyes | ss), 
-    data = iaf_res_long %>% filter(name == "cog"),
-    REML = TRUE
-  )
-summary(mod_2_cog)
-check_model(mod_2_cog)
-
-# Model comparison
-anova(mod_1_cog, mod_2_cog)
-
-
-# Visualization
-# subject-wise summary
+# summary: subject-wise
 iaf_res_long_ss <- 
   iaf_res_long %>% 
-  group_by(ss, eyes, name) %>% 
+  filter(complete.cases(value)) %>%
+  group_by(ss, eyes, name) %>%
   summarise(m = mean(value), n = n()) %>%
   ungroup()
 
-# sample-wise summary
-iaf_res_long_sum <-
+# summary: sample-wise
+iaf_res_long_sum <- 
   iaf_res_long_ss %>%
   group_by(eyes, name) %>%
-  summarise(M = mean(m), SD = sd(m), N = n(), SEM = SD/sqrt(N)) %>%
+  summarise(
+    M = mean(m), 
+    SD = sd(m), 
+    N = n(), 
+    SEM = SD/sqrt(N), 
+    LL = quantile(m, .025), 
+    UL = quantile(m, .975)
+    ) %>%
   ungroup()
 
-# plot
-pn <- position_nudge(x = .4)
+# visualization
+pn <- position_nudge(x = .3)
 pj <- position_jitter(width = .1, height = 0)
-ggplot(iaf_res_long_sum, aes(eyes, M, fill = name)) +
-  geom_bar(stat = "identity", width = .3, color = "black", position = pn) +
-  geom_errorbar(aes(ymin=M-SEM, ymax = M+SEM), width = .1, position = pn) +
+ggplot(iaf_res_long_sum, aes(eyes, M, color = name, fill = name)) +
   geom_point(
     data = iaf_res_long_ss, 
-    aes(y = m, color = name), 
-    alpha = 1/2,
-    position = pj,
-    shape = 16
+    aes(y = m), 
+    position = pj, 
+    alpha = 1/3,
+    shape = 16) +
+  geom_bar(stat = "identity", width = .25, color = "black", position = pn) +
+  geom_errorbar(
+    aes(ymin=M-SEM, ymax = M+SEM), 
+    width = .1, 
+    color = "black", 
+    position = pn
     ) +
+  coord_cartesian(ylim = c(8, 13)) +
   scale_fill_manual(
     values = c(
       ghibli_palettes$MononokeLight[3], ghibli_palettes$MononokeLight[5]
@@ -107,8 +75,90 @@ ggplot(iaf_res_long_sum, aes(eyes, M, fill = name)) +
       ghibli_palettes$MononokeLight[3], ghibli_palettes$MononokeLight[5]
     )
   ) +
-  coord_cartesian(ylim = c(7.5, 13)) +
-  labs(x = "Condition", y = "Individual Alpha Frequency", caption = "SEM error bars.") +
-  theme_bw() +
-  theme(legend.position = "none") +
-  facet_wrap(~name)
+  labs(x = "Eyes", y = "Frequency (Hz)", caption = "SEM error bars.") +
+  theme_bw() + 
+  facet_wrap(~name) +
+  theme(legend.position = "none")
+
+# Linear mixed effects modeling - - - -
+
+# Sets contrast (open > closed)
+contrasts(iaf_res_long$eyes) <- cbind(open_vs_closed = c(-.5, .5))
+
+
+# PEAK ALPHA FREQUENCY (via peak picking)
+
+# Minimal model
+mod_1_paf <- 
+  lmer(
+    value ~ 1 + block_mc*eyes + (1 | ss), 
+    data = iaf_res_long %>% filter(name == "paf"),
+    REML = TRUE,
+    control = lmerControl(optimizer = "bobyqa")
+  )
+summary(mod_1_paf)
+check_model(mod_1_paf)
+
+mod_2_paf <-
+  lmer(
+    value ~ 1 + block_mc*eyes + (1 + block_mc + eyes | ss), 
+    data = iaf_res_long %>% filter(name == "paf"),
+    REML = TRUE,
+    control = lmerControl(optimizer = "bobyqa")
+  )
+summary(mod_2_paf)
+check_model(mod_2_paf)
+
+# original optimizer was not converging; switched to bobyqa
+# mod_2_paf_allfit <- allFit(mod_2_paf)
+# mod_2__paf_allfit_OK <- mod_2_paf_allfit[sapply(mod_2_paf_allfit, is, "merMod")]
+# lapply(mod_2__paf_allfit_OK, function(x) x@optinfo$conv$lme4$messages)
+
+mod_3_paf <-
+  lmer(
+    value ~ 1 + block_mc*eyes + (1 + block_mc*eyes | ss), 
+    data = iaf_res_long %>% filter(name == "paf"),
+    REML = TRUE,
+    control = lmerControl(optimizer = "bobyqa")
+  )
+summary(mod_3_paf)
+check_model(mod_3_paf)
+
+# Plots interaction
+interactions::interact_plot(
+  mod_3_paf, 
+  pred = block_mc, 
+  modx = eyes, 
+  plot.points = TRUE
+)
+
+
+# Model comparison
+anova(mod_1_paf, mod_2_paf, mod_3_paf)
+
+# CENTER OF GRAVITY (essentially a weighted average)
+
+# Minimal model
+mod_1_cog <- 
+  lmer(
+    value ~ 1 + block_mc*eyes + (1 | ss), 
+    data = iaf_res_long %>% filter(name == "cog"),
+    REML = TRUE
+  )
+summary(mod_1_cog)
+check_model(mod_1_cog)
+
+# block_mc does not have sufficient variability
+mod_2_cog <-
+  lmer(
+    value ~ 1 + block_mc*eyes + (1 + eyes | ss), 
+    data = iaf_res_long %>% filter(name == "cog"),
+    REML = TRUE
+  )
+summary(mod_2_cog)
+check_model(mod_2_cog)
+
+# Model comparison
+anova(mod_1_cog, mod_2_cog)
+
+
---
+++ api-calls.R
@@ -0,0 +1,267 @@
+# API Calls for CRAMPP
+# Matt Kmiecik
+# Started: 13 SEPTEMBER 2022
+
+# Purpose: run this script to refresh Redcap annual data
+
+# Loads packages - - - -
+source("r-prep.R") # Prepares R workspace
+
+# API setup - - - -
+api_url <- "https://survey.northshore.org/api/" # Redcap URL
+
+# Tokens
+arm1_api_token <- 
+  read_lines(
+    file = "../private/EH13-094-OCP-GRANT-api-token.txt" # token
+  )
+
+arm2_api_token <- 
+  read_lines(
+    file = "../private/EH13-094-Arm-2-api-token.txt" # token
+  )
+
+shortened_annual_api_token <- 
+  read_lines(
+    file = "../private/EH13-094-Shortened-Annual-api-token.txt" # token
+  )
+
+# Function for API calls - - - -
+# PROBLEM IS HERE: https://education.arcus.chop.edu/redcap-r-windows/
+
+#########################################
+#                                       #
+# Grabs annuals from EH13-094-OCP-GRANT #
+#                                       #
+#########################################
+
+arm1_annuals <- 
+  POST(
+    url = api_url,
+    body = list(
+      token = arm1_api_token,
+      content = "record",
+      format = "csv",
+      type = "eav", # for longitudinal output
+      "forms[0]" = "annual_questionnaire_combined060416", # form
+      rawOrLabel = "raw",
+      rawOrLabelHeaders = "raw",
+      exportCheckboxLabel = "false",
+      exportSurveyFields = "true", # reports timestamps etc.
+      exportDataAccessGroups = "false",
+      returnFormat = "csv"
+    )
+  )
+# parses .$content
+arm1_annuals_parsed <- content(arm1_annuals, as = "parsed")
+
+# Trying to get time stamps of assessment visit
+# pulls all data bc why not
+arm1_avisit1_api <- 
+  POST(
+    url = api_url,
+    body = list(
+      token = arm1_api_token,
+      content = "record",
+      format = "csv",
+      type = "eav", # for longitudinal output
+      #"forms[0]" = "ic_problem_and_symptoms_indices",
+      "events[0]" = "assessment_visit_1_arm_1",
+      rawOrLabel = "raw",
+      rawOrLabelHeaders = "raw",
+      exportCheckboxLabel = "false",
+      exportSurveyFields = "true", # reports timestamps etc.
+      exportDataAccessGroups = "false",
+      returnFormat = "csv"
+    )
+  )
+# parses .$content
+arm1_avisit1_api_parsed <- content(arm1_avisit1_api, as = "parsed")
+
+# SCREEN VISIT DATA
+arm1_screenvisit_api <- 
+  POST(
+    url = api_url,
+    body = list(
+      token = arm1_api_token,
+      content = "record",
+      format = "csv",
+      type = "eav", # for longitudinal output
+      "events[0]" = "screen_visit_arm_1",
+      rawOrLabel = "raw",
+      rawOrLabelHeaders = "raw",
+      exportCheckboxLabel = "false",
+      exportSurveyFields = "true", # reports timestamps etc.
+      exportDataAccessGroups = "false",
+      returnFormat = "csv"
+    )
+  )
+# parses .$content
+arm1_screenvisit_api_parsed <- content(arm1_screenvisit_api, as = "parsed")
+
+# NON-STANDARD FOLLOWUP
+# Some participants were given original questionnaires at year 1 followup
+# these variables do not start with an "a"
+arm1_year1_followup <- 
+  POST(
+    url = api_url,
+    body = list(
+      token = arm1_api_token,
+      content = "record",
+      format = "csv",
+      type = "eav", # for longitudinal output
+      "events[0]" = "year_1_followup_do_arm_1",  
+      rawOrLabel = "raw",
+      rawOrLabelHeaders = "raw",
+      exportCheckboxLabel = "false",
+      exportSurveyFields = "true", # reports timestamps etc.
+      exportDataAccessGroups = "false",
+      returnFormat = "csv"
+    )
+  )
+# parses .$content
+arm1_year1_followup_parsed <- content(arm1_year1_followup, as = "parsed")
+
+
+#########################################
+#                                       #
+# Grabs annuals from EH13-094- Arm 2    #
+#                                       #
+#########################################
+
+arm2_annuals <- 
+  POST(
+    url = api_url,
+    body = list(
+      token = arm2_api_token,
+      content = "record",
+      format = "csv",
+      type = "eav", # for longitudinal output
+      "forms[0]" = "annual_questionnaire_combined060416", # form annual_questionnaire_combined060416
+      rawOrLabel = "raw",
+      rawOrLabelHeaders = "raw",
+      exportCheckboxLabel = "false",
+      exportSurveyFields = "true", # reports timestamps etc.
+      exportDataAccessGroups = "false",
+      returnFormat = "csv"
+    )
+  )
+# parses .$content
+arm2_annuals_parsed <- content(arm2_annuals, as = "parsed")
+
+# Trying to get time stamps of assessment visit
+# pulls all avisit 1 data bc why not
+arm2_avisit1_api <- 
+  POST(
+    url = api_url,
+    body = list(
+      token = arm2_api_token,
+      content = "record",
+      format = "csv",
+      type = "eav", # for longitudinal output
+      #"forms[0]" = "ic_problem_and_symptoms_indices",
+      "events[0]" = "assessment_visit_1_arm_1",
+      rawOrLabel = "raw",
+      rawOrLabelHeaders = "raw",
+      exportCheckboxLabel = "false",
+      exportSurveyFields = "true", # reports timestamps etc.
+      exportDataAccessGroups = "false",
+      returnFormat = "csv"
+    )
+  )
+# parses .$content
+arm2_avisit1_api_parsed <- content(arm2_avisit1_api, as = "parsed")
+
+# SCREEN VISIT DATA
+arm2_screenvisit_api <- 
+  POST(
+    url = api_url,
+    body = list(
+      token = arm2_api_token,
+      content = "record",
+      format = "csv",
+      type = "eav", # for longitudinal output
+      #"forms[0]" = "ic_problem_and_symptoms_indices",
+      "events[0]" = "screen_visit_arm_1",
+      rawOrLabel = "raw",
+      rawOrLabelHeaders = "raw",
+      exportCheckboxLabel = "false",
+      exportSurveyFields = "true", # reports timestamps etc.
+      exportDataAccessGroups = "false",
+      returnFormat = "csv"
+    )
+  )
+# parses .$content
+arm2_screenvisit_api_parsed <- content(arm2_screenvisit_api, as = "parsed")
+
+
+#######################################
+#                                     #
+# EH13-094 Shortened Annual Follow Up #
+#                                     #
+#######################################
+
+short_annuals <- 
+  POST(
+    url = api_url,
+    body = list(
+      token = shortened_annual_api_token,
+      content = "record",
+      format = "csv",
+      type = "eav", # for longitudinal output
+      "forms[0]" = "shortened_annual_questionnaire", # 
+      rawOrLabel = "raw",
+      rawOrLabelHeaders = "raw",
+      exportCheckboxLabel = "false",
+      exportSurveyFields = "true", # reports timestamps etc.
+      exportDataAccessGroups = "false",
+      returnFormat = "csv"
+    )
+  )
+# parses .$content
+short_annuals_parsed <- content(short_annuals, as = "parsed")
+
+# Saves out API data - - - -
+
+# ARM 1 ANNUAL DATA
+save(arm1_annuals_parsed, file = "../output/arm1_annuals_parsed.RData")
+# ARM 1 ASSESSMENT VISIT 1 DATA 
+save(arm1_avisit1_api_parsed, file = "../output/arm1_avisit1_api_parsed.RData")
+# ARM 1 SCREEN VISIT DATA
+save(arm1_screenvisit_api_parsed, file = "../output/arm1_screenvisit_api_parsed.RData")
+# ARM 1 Year 1 FOLLOW UP AS ASSESSMENT VISIT 1
+save(arm1_year1_followup_parsed, file = "../output/arm1_year1_followup_parsed.RData")
+
+# ARM 2 ANNUAL DATA
+save(arm2_annuals_parsed, file = "../output/arm2_annuals_parsed.RData")
+# ARM 2 ASSESSMENT VISIT 1 DATA
+save(arm2_avisit1_api_parsed, file = "../output/arm2_avisit1_api_parsed.RData")
+# ARM 2 SCREEN VISIT DATA
+save(arm2_screenvisit_api_parsed, file = "../output/arm2_screenvisit_api_parsed.RData")
+
+# SHORTENED ANNUALS DATA
+save(short_annuals_parsed, file = "../output/short_annuals_parsed.RData")
+
+# Cleaning script objects - - - -
+rm(
+  arm1_annuals,
+  arm1_annuals_parsed,
+  arm1_avisit1_api,
+  arm1_avisit1_api_parsed,
+  arm2_annuals,
+  arm2_annuals_parsed,
+  arm2_avisit1_api,
+  arm2_avisit1_api_parsed,
+  short_annuals,
+  short_annuals_parsed,
+  api_url,
+  arm1_api_token,
+  arm2_api_token,
+  shortened_annual_api_token,
+  arm1_screenvisit_api,
+  arm1_screenvisit_api_parsed,
+  arm2_screenvisit_api,
+  arm2_screenvisit_api_parsed,
+  arm1_year1_followup,
+  arm1_year1_followup_parsed
+)
